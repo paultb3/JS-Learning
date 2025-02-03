d5c1f8f355eb5a6ecf796be5791645ceb9f1a777
paultb3 <paulbenancio3000@gmail.com>
2025-02-03 12:25:42 -0500

    Primer dia de JS, terminado

---
+++ 1.DiaUno/variables.js
@@ -0,0 +1,30 @@
+//Asi se realiza un comentario en JavaScript
+
+
+let a = 30; //La mayoria de los navegadores soportan esta inicilizacion de variables
+var c = 50; //Forma antigua de inicializar un avariable
+
+
+
+/*let b = 10;
+let d = 10;
+let e = 10;
+let f = 10;*/
+
+let b = 10, 
+    d = "I am: ", 
+    e = "Happy", 
+    f = 10, 
+    x = a + b;
+
+const permanente = d + x;
+
+
+
+//Las llaves significan que lo que se pasa es un objeto
+//console.log('%c Mis variables','color: blue; font-weight: bold; background: white');
+//console.log({a: x});
+//console.log({b: x});
+//console.log({c: x});
+//console.table({b,d,e,f,x});
+
